1ec9e3ce0f58e61a8464b6e749510a73c22383b4
Valentin Plechuc <vplechuc@gmail.com>
2014-11-13 16:12:38 -0300

    new definitions

--- demo.js
+++ demo.js
@@ -1,16 +1,36 @@
-var zendesk = require('node-zendesk'),
-    fs      = require('fs');
+var zd = require('node-zendesk'),
+    fs = require('fs');
 
-var client = zendesk.createClient({
-  username:  'username',
-  token:     'token',
-  remoteUri: 'https://remote.zendesk.com/api/v2'
+var client = zd.createClient({
+  debug:     true,
+  username:  'developer-support@versionone.com',
+  password:  'sJ5Z&VjZ',
+  remoteUri: 'https://versiononesupport.zendesk.com/agent/'
 });
 
-client.users.list(function (err, req, result) {
+//verify connection
+client.users.auth(function (err, res, result) {
   if (err) {
     console.log(err);
     return;
   }
-  console.log(JSON.stringify(result[0], null, 2, true));//gets the first page
+  console.log(JSON.stringify(result.verified, null, 2, true));
 });
+
+// client.users.list(function (err, req, result) {
+//    if (err) {
+//      console.log(err);
+//      return;
+//    }
+//    console.log(JSON.stringify(result[0], null, 2, true));//gets the first page
+//  });
+
+// display tickets
+// client.tickets.list(function (err, statusList, body, responseList, resultList) {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(JSON.stringify(body, null, 2, true));//will display all tickets
+// });
+
